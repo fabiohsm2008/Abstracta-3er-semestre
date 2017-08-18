4cfcda083032c84410647883ad4314b6d6c54baa
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-08-18 14:27:10 -0500

    Add files via upload

---
+++ Euclides second version.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int modulo(int a, int n)
+{
+	int q, r;
+	q = a / n;
+	r = a - (q*n);
+	if (r < 0)
+		r += n;
+	return r;
+}
+
+int euclides(int a, int b)
+{
+	int residuo = modulo(a,b);
+	while(residuo!=0)
+	{
+		cout << a << " = ";
+		a = b;
+		cout << "(" << a/b << ")" << b << " + " << residuo << endl;
+		b = residuo;
+		residuo = modulo(a,b);
+	}
+	cout << a << " = " << "(" << a/b << ")" << b << " + " << residuo << endl;
+	cout << "El maximo comun divisor es: " << b;
+	return b;
+}
+
+int main(int argc, char *argv[]) {
+	euclides(78,32);
+	return 0;
+}
+
